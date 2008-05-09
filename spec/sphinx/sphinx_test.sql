844a1ffecdc3fcd74b7ddb257d2d66960b51027b
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2008-05-09 17:23:36 +0000

    Added test for MVA

--- spec/sphinx/sphinx_test.sql
+++ spec/sphinx/sphinx_test.sql
@@ -27,6 +27,26 @@ CREATE TABLE `links` (
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/* Table structure for table `tags` */
+
+DROP TABLE IF EXISTS `tags`;
+
+CREATE TABLE `tags` (
+  `id` INT(11) NOT NULL auto_increment,
+  `tag` VARCHAR(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* Table structure for table `links_tags` */
+
+DROP TABLE IF EXISTS `links_tags`;
+
+CREATE TABLE `links_tags` (
+  `link_id` INT(11) NOT NULL,
+  `tag_id` INT(11) NOT NULL,
+  PRIMARY KEY  (`link_id`,`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 /* Table structure for table `links64` */
 
 DROP TABLE IF EXISTS `links64`;
@@ -47,6 +67,17 @@ INSERT INTO `links`(`id`,`name`,`created_at`,`description`,`group_id`,`rating`) 
 	(2,'Airplanes To Become WiFi Hotspots','2007-04-04 06:49:15','Airlines will start turning their airplanes into WiFi hotspots beginning early next year, WSJ reports. Here\'s what you need to know...',2,54.85),
 	(3,'Planet VIP-195 GSM/WiFi Phone With Windows Messanger','2007-04-04 06:50:47','The phone does comply with IEEE 802.11b and IEEE 802.11g to provide phone capability via WiFi. As GSM phone the VIP-195 support 900/1800/1900 band and GPRS too. It comes with simple button to switch between WiFi or GSM mod',1,16.25);
 
+/* Data for the table `tags` */
+INSERT INTO `tags`(`id`,`tag`) VALUES
+	(1, 'tag1'),(2, 'tag2'),(3, 'tag3'),(4, 'tag4'),(5, 'tag5'),
+	(6, 'tag6'),(7, 'tag7'),(8, 'tag8'),(9, 'tag9'),(10, 'tag5');
+
+/* Data for the table `links_tags` */
+INSERT INTO `links_tags`(`link_id`,`tag_id`) VALUES
+	(1, 1),(1, 2),(1, 3),(1, 4),
+	(2, 5),(2, 6),(2, 7),(2, 8),
+	(3, 9),(3, 1),(3, 7),(3, 10);
+	
 /* Data for the table `links64` */
 
 INSERT INTO `links64`(`id`,`name`,`created_at`,`description`,`group_id`) VALUES
