4bd1203a1e0919d193443e04c0de5bc9c45c8069
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2008-05-09 17:23:18 +0000

    Added class to parse response stream.

--- lib/sphinx.rb
+++ lib/sphinx.rb
@@ -1,5 +1,6 @@
-require File.dirname(__FILE__) + '/sphinx/client'
 require File.dirname(__FILE__) + '/sphinx/request'
+require File.dirname(__FILE__) + '/sphinx/response'
+require File.dirname(__FILE__) + '/sphinx/client'
 
 module Sphinx
 end
