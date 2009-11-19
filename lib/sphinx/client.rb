162e352905f7007021d1f90297a79daf3ac113c1
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2009-11-19 12:07:14 +0200

    Version bump to 0.9.10.2086

--- lib/sphinx/client.rb
+++ lib/sphinx/client.rb
@@ -3,7 +3,7 @@
 # Author::    Dmytro Shteflyuk <mailto:kpumuk@kpumuk.info>.
 # Copyright:: Copyright (c) 2006 — 2009 Dmytro Shteflyuk
 # License::   Distributes under the same terms as Ruby
-# Version::   0.9.10-r2043
+# Version::   0.9.10-r2086
 # Website::   http://kpumuk.info/projects/ror-plugins/sphinx
 #
 # This library is distributed under the terms of the Ruby license.
